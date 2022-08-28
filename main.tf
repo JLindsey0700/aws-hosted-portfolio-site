40acf4a79464fdadc58eebd54a8531021d971195
JLindsey0700 <james.l.8182@gmail.com>
2022-08-28 23:14:06 +1200

    Updated subnet configuration, added security group for instances

--- main.tf
+++ main.tf
@@ -16,77 +16,95 @@ provider "aws" {
 }
 
 # Creates the main VPC 
-resource "aws_vpc" "main-vpc" {
+resource "aws_vpc" "main_vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
   tags = {
-    Name = "main-vpc"
-  }
+    Name = "main_vpc"  
 }
+}
+
 # Creates two public and private subnets
-resource "aws_subnet" "public-a" {
-  vpc_id     = aws_vpc.main-vpc.id
-  cidr_block = "10.0.0.0/26"
+resource "aws_subnet" "public_a" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "us-east-1a"
 
   tags = {
-    Name = "public-a"
+    Name = "public_a"
   }
 }
-resource "aws_subnet" "public-b" {
-  vpc_id     = aws_vpc.main-vpc.id
-  cidr_block = "10.0.0.64/26"
+resource "aws_subnet" "public_b" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1b"
 
   tags = {
-    Name = "public-b"
+    Name = "public_b"
   }
 }
-resource "aws_subnet" "private-a" {
-  vpc_id     = aws_vpc.main-vpc.id
-  cidr_block = "10.0.0.128/26"
-
+resource "aws_subnet" "private_a" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
   tags = {
-    Name = "private-a"
+    Name = "private_a"
   }
 }
-resource "aws_subnet" "private-b" {
-  vpc_id     = aws_vpc.main-vpc.id
-  cidr_block = "10.0.0.192/26"
-
+resource "aws_subnet" "private_b" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "us-east-1b"
   tags = {
-    Name = "private-b"
+    Name = "private_b"
   }
 }
 # Creates IGW in main VPC
-resource "aws_internet_gateway" "main-igw" {
-  vpc_id = aws_vpc.main-vpc.id
+resource "aws_internet_gateway" "main_igw" {
+  vpc_id = aws_vpc.main_vpc.id
 
   tags = {
-    Name = "main-igw"
+    Name = "main_igw"
   }
 }
-# Attaches IGW to main VPC
-resource "aws_internet_gateway_attachment" "igw-vpc" {
-  internet_gateway_id = aws_internet_gateway.main-igw.id
-  vpc_id              = aws_vpc.main-vpc.id
-}
+
 # Creates route table for main VPC
-resource "aws_route_table" "main-rt" {
-  vpc_id = aws_vpc.main-vpc.id
+resource "aws_route_table" "main_rt" {
+  vpc_id = aws_vpc.main_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main-igw.id
+    gateway_id = aws_internet_gateway.main_igw.id
   }
   tags = {
-    Name = "example"
+    Name = "main_RT"
   }
+}
+# Assosiates main rt with the main vpc
+resource "aws_main_route_table_association" "rt_association" {
+  vpc_id         = aws_vpc.main_vpc.id
+  route_table_id = aws_route_table.main_rt.id
 }
 # Assosiates the subnets with the route table
 resource "aws_route_table_association" "subnet-rt-pubA" {
-  subnet_id      = aws_subnet.public-a.id
-  route_table_id = aws_route_table.main-rt.id
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.main_rt.id
 }
 resource "aws_route_table_association" "subnet-rt-pubB" {
-  subnet_id      = aws_subnet.public-b.id
-  route_table_id = aws_route_table.main-rt.id
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.main_rt.id
 }
+
+# Creates public security group for the static web hosted EC2 instance
+resource "aws_security_group" "web_sg" {
+  name        = "web_sg"
+  description = "Web Security Group for HTTP"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
