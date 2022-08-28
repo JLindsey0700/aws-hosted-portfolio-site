57980a3b28dd8bf0172aa04f91f53ef46569015e
JLindsey0700 <james.l.8182@gmail.com>
2022-08-29 00:30:38 +1200

    Added the instance which will host the website, edited security group rules to allow all inbound and outbound internet access

--- main.tf
+++ main.tf
@@ -19,6 +19,7 @@ provider "aws" {
 resource "aws_vpc" "main_vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
+  enable_dns_hostnames = true
   tags = {
     Name = "main_vpc"  
 }
@@ -29,7 +30,7 @@ resource "aws_subnet" "public_a" {
   vpc_id     = aws_vpc.main_vpc.id
   cidr_block = "10.0.0.0/24"
   availability_zone = "us-east-1a"
-
+  map_public_ip_on_launch = true
   tags = {
     Name = "public_a"
   }
@@ -38,8 +39,8 @@ resource "aws_subnet" "public_b" {
   vpc_id     = aws_vpc.main_vpc.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-1b"
-
-  tags = {
+  map_public_ip_on_launch = true
+    tags = {
     Name = "public_b"
   }
 }
@@ -62,16 +63,13 @@ resource "aws_subnet" "private_b" {
 # Creates IGW in main VPC
 resource "aws_internet_gateway" "main_igw" {
   vpc_id = aws_vpc.main_vpc.id
-
   tags = {
     Name = "main_igw"
   }
 }
-
 # Creates route table for main VPC
 resource "aws_route_table" "main_rt" {
   vpc_id = aws_vpc.main_vpc.id
-
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.main_igw.id
@@ -98,13 +96,40 @@ resource "aws_route_table_association" "subnet-rt-pubB" {
 # Creates public security group for the static web hosted EC2 instance
 resource "aws_security_group" "web_sg" {
   name        = "web_sg"
-  description = "Web Security Group for HTTP"
+  description = "Web Security Group"
+  vpc_id = aws_vpc.main_vpc.id
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol = -1
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
+# Creates an EC2 instance which will host the static web site
+resource "aws_instance" "static_website" {
+  ami = "ami-05fa00d4c63e32376"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.public_a.id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  key_name = "staticwebserver"
+  # Bootstrapping the EC2 instance with apache webserver, running systemctl manually with sudo seems to make it work
+  user_data = <<EOF
+#!/bin/bash
+yum update -y
+yum install -y httpd.x86_64
+systemctl start httpd.service 
+systemctl enable httpd.service
+echo “Hello World from $(hostname -f)” > /var/www/html/index.html"
 
+EOF
+  tags = {
+    Name = "static_website"
+  }
+}
